41c496417b8c6166bdb523713f9ce8a9a585e2c0
lauvel19 <lgvelez_159@cue.edu.co>
2025-07-15 11:01:36 -0500

    implementación completa de perfiles de usuario y soporte, registro de nuevos tickets, listado general y vista detallada

---
+++ models/Ticket.php
@@ -0,0 +1,149 @@
+<?php
+    class Ticket extends Conectar{
+
+        public function insert_ticket($usu_id,$cat_id,$tick_titulo,$tick_descrip){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO tm_ticket (tick_id,usu_id,cat_id,tick_titulo,tick_descrip,tick_estado,fech_crea,est) VALUES (NULL,?,?,?,?,'Abierto',now(),'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $usu_id);
+            $sql->bindValue(2, $cat_id);
+            $sql->bindValue(3, $tick_titulo);
+            $sql->bindValue(4, $tick_descrip);
+            $sql->execute();
+
+            $sql1="select last_insert_id() as 'tick_id';";
+            $sql1=$conectar->prepare($sql1);
+            $sql1->execute();
+            return $resultado=$sql1->fetchAll(pdo::FETCH_ASSOC);
+        }
+        
+        public function listar_ticket_x_usu($usu_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT 
+                tm_ticket.tick_id,
+                tm_ticket.usu_id,
+                tm_ticket.cat_id,
+                tm_ticket.tick_titulo,
+                tm_ticket.tick_descrip,
+                tm_ticket.tick_estado,
+                tm_ticket.fech_crea,
+                tm_usuario.usu_nom,
+                tm_usuario.usu_ape,
+                tm_categoria.cat_nom
+                FROM 
+                tm_ticket
+                INNER join tm_categoria on tm_ticket.cat_id = tm_categoria.cat_id
+                INNER join tm_usuario on tm_ticket.usu_id = tm_usuario.usu_id
+                WHERE
+                tm_ticket.est = 1
+                AND tm_usuario.usu_id=?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $usu_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+         public function listar_ticket_x_id($tick_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT 
+                tm_ticket.tick_id,
+                tm_ticket.usu_id,
+                tm_ticket.cat_id,
+                tm_ticket.tick_titulo,
+                tm_ticket.tick_descrip,
+                tm_ticket.tick_estado,
+                tm_ticket.fech_crea,
+                tm_usuario.usu_nom,
+                tm_usuario.usu_ape,
+                tm_usuario.usu_correo,
+                tm_categoria.cat_nom
+                FROM 
+                tm_ticket
+                INNER join tm_categoria on tm_ticket.cat_id = tm_categoria.cat_id
+                INNER join tm_usuario on tm_ticket.usu_id = tm_usuario.usu_id
+                WHERE
+                tm_ticket.est = 1
+                AND tm_ticket.tick_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $tick_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function listar_ticket(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT
+                tm_ticket.tick_id,
+                tm_ticket.usu_id,
+                tm_ticket.cat_id,
+                tm_ticket.tick_titulo,
+                tm_ticket.tick_descrip,
+                tm_ticket.tick_estado,
+                tm_ticket.fech_crea,
+                tm_usuario.usu_nom,
+                tm_usuario.usu_ape,
+                tm_categoria.cat_nom
+                FROM 
+                tm_ticket
+                INNER join tm_categoria on tm_ticket.cat_id = tm_categoria.cat_id
+                INNER join tm_usuario on tm_ticket.usu_id = tm_usuario.usu_id
+                WHERE
+                tm_ticket.est = 1
+                ";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function listar_ticketdetalle_x_ticket($tick_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT
+                td_ticketdetalle.tickd_id,
+                td_ticketdetalle.tickd_descrip,
+                td_ticketdetalle.fech_crea,
+                tm_usuario.usu_nom,
+                tm_usuario.usu_ape,
+                tm_usuario.rol_id
+                FROM 
+                td_ticketdetalle
+                INNER join tm_usuario on td_ticketdetalle.usu_id = tm_usuario.usu_id
+                WHERE 
+                tick_id =?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $tick_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function insert_ticketdetalle($tick_id,$usu_id,$tickd_descrip){
+            $conectar= parent::conexion();
+            parent::set_names();
+                $sql="INSERT INTO td_ticketdetalle (tickd_id,tick_id,usu_id,tickd_descrip,fech_crea,est) VALUES (NULL,?,?,?,now(),'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $tick_id);
+            $sql->bindValue(2, $usu_id);
+            $sql->bindValue(3, $tickd_descrip);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function update_ticket($tick_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="update tm_ticket 
+                set	
+                    tick_estado = 'Cerrado'
+                where
+                    tick_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $tick_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+    }
+?>
---
+++ view/DetalleTicket/detalleticket.js
@@ -0,0 +1,67 @@
+function init(){
+
+}
+
+$(document).ready(function(){
+     var tick_id = getUrlParameter('ID');
+
+      $.post("../../controller/ticket.php?op=listardetalle", { tick_id : tick_id }, function (data) {
+        $('#lbldetalle').html(data);
+    }); 
+
+    $.post("../../controller/ticket.php?op=mostrar", { tick_id : tick_id }, function (data) {
+        data = JSON.parse(data);
+        $('#lblestado').html(data.tick_estado);
+        $('#lblnomusuario').html(data.usu_nom +' '+data.usu_ape);
+        $('#lblfechcrea').html(data.fech_crea);
+
+        $('#lblnomidticket').html("Detalle Ticket -" + data.tick_id);
+    }); 
+
+    $('#tickd_descrip').summernote({
+        height: 300,
+        lang: "es-ES",
+        callbacks: {
+            onImageUpload: function(image) {
+                console.log("Image detect...");
+                myimagetreat(image[0]);
+            },
+            onPaste: function (e) {
+                console.log("Text detect...");
+            }
+        }
+    });
+
+     $('#tickd_descripusu').summernote({
+        height: 400,
+        lang: "es-ES",
+        toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ]
+    });  
+
+    $('#tickd_descripusu').summernote('disable');
+
+});
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+init();
